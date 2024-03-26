424969b46043d8c29f6bf87882e56276188f416a
Aamir Khan <158450707+aamirkhankhushk@users.noreply.github.com>
2024-03-26 13:48:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+//Temperature Conversion
+const textBox = document.getElementById("textBox");
+const toFahernheit = document.getElementById("toFahernheit");
+const toCelcius = document.getElementById("toCelcius");
+const result = document.getElementById("result");
+let temp; 
+
+function convert(){
+    if(toFahernheit.checked){
+        temp = Number(textBox.value);
+        temp = temp * 9 / 5 + 32;   
+        result.textContent = temp.toFixed(1) + "°F";
+
+    }else if (toCelcius.checked){
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5/9);   
+        result.textContent = temp.toFixed(1) + "°C";
+    }
+    else{
+        result.textContent = "Select a unit";
+
+    }
+
+    
+
+}
